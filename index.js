3b1a701f56ed8382aeb1ce8b3078fb8dd79cb6bb
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2021-10-26 19:02:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const PORT = 6060;
 
 app.get('/hear', function (req, res) {
   if (req.query.text !== "" && req.query.lang !== "") {
-
+    try {
+      console.log("I understand, wait a few moments, file being processed !");
+      try {
+        const gtts = new gTTS(req.query.text, req.query.lang);
+      	console.log("Your file is processed !");
+      } catch (err) {
+      	console.error(err);
+      }
+    } catch (e) {
+      console.log("err ", e);
+      res.status(404)
+    }
   } else {
     console.log("errr: lang or text is not valid");
   }
