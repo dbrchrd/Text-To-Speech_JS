bfe012d33e5daf93d299e68ae0e161aefc748c10
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2021-10-26 19:02:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express();
 const gTTS = require('gtts');
 const PORT = 6060;
 
+app.get('/hear', function (req, res) {
+  if (req.query.text !== "" && req.query.lang !== "") {
+
+  } else {
+    console.log("errr: lang or text is not valid");
+  }
+});
+
 app.listen(PORT, function () {
-  console.log('Open url to hear Baguette http://localhost:3000/hear?lang=fr&text=Baguette');
+  console.log(`Open url to hear Baguette http://localhost:${PORT}/hear?lang=fr&text=Baguette`);
 });
